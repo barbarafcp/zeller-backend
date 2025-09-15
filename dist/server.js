eb97174c79f6f05d7afae8ae69274e387a7adcfb
Bárbara Cabezas <barbara@Barbaras-MacBook-Air.local>
2025-09-15 14:41:23 -0300

    feat: add basic Koa server with logger, body parser and router

--- dist/server.js
+++ dist/server.js
@@ -3,19 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// Import the 'express' module
-const express_1 = __importDefault(require("express"));
-// Create an Express application
-const app = (0, express_1.default)();
-// Set the port number for the server
+const koa_1 = __importDefault(require("koa"));
+const koa_logger_1 = __importDefault(require("koa-logger"));
+const koa_body_1 = __importDefault(require("koa-body"));
+const routes_1 = __importDefault(require("./routes/routes"));
+// Create a koa application
+const app = new koa_1.default();
 const port = 3000;
-// Define a route for the root path ('/')
-app.get('/', (req, res) => {
-    // Send a response to the client
-    res.send('Hello, TypeScript + Node.js + Express!');
-});
+app.use((0, koa_logger_1.default)());
+app.use((0, koa_body_1.default)());
+app.use(routes_1.default.routes());
+app.use((ctx, next) => (ctx.body = "Hello"));
 // Start the server and listen on the specified port
 app.listen(port, () => {
-    // Log a message when the server is successfully running
     console.log(`Server is running on http://localhost:${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,22 @@
-// Import the 'express' module along with 'Request' and 'Response' types from express
-import express, { Request, Response } from 'express';
+import koa from 'koa';
+import koaLogger from 'koa-logger';
+import koaBody from 'koa-body';
+import router from './routes/routes';
 
-// Create an Express application
-const app = express();
-
-// Specify the port number for the server
+// Create a koa application
+const app = new koa();
 const port: number = 3000;
 
-// Define a route for the root path ('/')
-app.get('/', (req: Request, res: Response) => {
-  // Send a response to the client
-  res.send('Hello, TypeScript + Node.js + Express!');
-});
+app.use(koaLogger());
+app.use(koaBody());
+
+app.use(router.routes());
+
+app.use((ctx, next) => (
+    ctx.body = "Hello"
+));
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
-  // Log a message when the server is successfully running
   console.log(`Server is running on http://localhost:${port}`);
 });
